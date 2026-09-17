35439fbb0bc2f7853d538adb8eec067d29fec0c8
buantuevaviktoria <viktoriabuantuevi@mail.ru>
2026-09-17 16:07:53 +0500

    Task1

--- obj/Debug/net8.0/Tyuiu.BuantuevaVA.Sprint0.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tyuiu.BuantuevaVA.Sprint0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tyuiu.BuantuevaVA.Sprint0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57757ac8cdccc3d0d2f04ca2ce595e192ee7c1e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tyuiu.BuantuevaVA.Sprint0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tyuiu.BuantuevaVA.Sprint0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
